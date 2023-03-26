b428de6196b611b9b506c261affefdc0899f2491
kabornblack <shaibu.kabiru@email.com>
2023-03-26 19:46:18 +0300

    Added features to redirect the compose page to the home page after making a post

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+let posts = [];
 
 app.get("/", function(req, res){
-  res.render("home", {homeContent: homeStartingContent});
+  res.render("home", {homeContent: homeStartingContent, posts: posts});
+
 });
 
 app.get("/about", function(req, res){
@@ -28,6 +29,21 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+
 
 
 
